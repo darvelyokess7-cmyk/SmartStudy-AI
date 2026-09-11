013983240fae42a3561e98338b5cc06c064b7666
Davalon <darvelyokess7@gmail.com>
2026-09-12 01:47:20 +0300

    Fix PORT variable for Renderdeployment

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import app from "./app";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(` Server is running on http://localhost:${PORT}`);
